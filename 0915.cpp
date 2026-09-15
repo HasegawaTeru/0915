8d23c308185de07e81b8de05cd0a6921aabf3fc8
4D-12\koyo <a>
2026-09-15 13:33:29 +0900

    GameManagerクラスの実装

--- 0915.cpp
+++ 0915.cpp
@@ -1,20 +1,39 @@
-﻿// 0915.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
+﻿#include <iostream>
 
-#include <iostream>
+class GameManager
+{
+	std::unique_ptr<GameState>currentState;
+	bool isRunning;
+	float gameTime;
+public:
+	GameManager() : isRunning(true), gameTime(0.0f){}
+	void ChangeState(std::unique_ptr<GameState> newState)
+	{
+		currentState->OnExit(this);
+		currentState = std::move(newState);
+		currentState->OnEnter(this);
+	}
+	void Update(float deltaTime)
+	{
+		gameTime += deltaTime;
+		if (currentState)
+		{
+			currentState->OnUpdate(this, deltaTime);
+		}
+	}
+};
+
+class GameState
+{
+public:
+	virtual ~GameState() = default;
+	virtual void OnEnter(GameManager* manager) = 0;
+	virtual void OnUpdate(GameManager* manager, float deltaTime) = 0;
+	virtual void OnExit(GameManager* manager) = 0;
+	virtual const std::string GetName() const = 0;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
